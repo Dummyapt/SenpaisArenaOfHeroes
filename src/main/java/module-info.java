30b5d31527b40a7047a3772a1f654a05a719cef3
Dummyapt <support@dummyapt.de>
2023-06-01 12:27:40 +0200

    Version angepasst und unnötigen Export entfernt

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,4 @@ module SenpaisArenaOfHeroes {
     exports de.dummyapt.senpaisarenaofheroes.hero;
     exports de.dummyapt.senpaisarenaofheroes.fiend;
     exports de.dummyapt.senpaisarenaofheroes.gui;
-    exports de.dummyapt.senpaisarenaofheroes;
 }
